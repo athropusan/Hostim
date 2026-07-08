cc9ea552969e509d4a6ea8f0f26336f6c93c495b
athopus <athopusoriginal@gmail.com>
2026-07-08 11:27:04 -0300

    Alterando nome do site no HTML

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// --- CONFIGURAÇÃO ---
 const WORKER_URL = 'https://gerador-rcc-upload.athropusoriginal.workers.dev';
 const R2_BASE_URL = "https://pub-387b66f2a9024194a685cf8a80608f27.r2.dev/";
 
-// Dispara o upload ao selecionar arquivos
 document.getElementById('botaoEnviar').addEventListener('click', () => {
     document.getElementById('imagemUpload').click();
 });
@@ -17,11 +15,10 @@ document.getElementById('imagemUpload').addEventListener('change', async functio
     botao.innerText = "Enviando...";
 
     try {
-        const albumId = crypto.randomUUID(); // Gera um ID único para o álbum
+        const albumId = crypto.randomUUID();
         let linksDasImagens = [];
         let miniaturasHTML = '';
 
-        // 1. Faz o upload das imagens uma por uma
         for (let arquivo of arquivos) {
             const resposta = await fetch(`${WORKER_URL}/`, {
                 method: 'POST',
@@ -34,19 +31,17 @@ document.getElementById('imagemUpload').addEventListener('change', async functio
             miniaturasHTML += `<img src="${linkCompleto}" class="thumb" onclick="abrirModal('${linkCompleto}')">`;
         }
 
-        // 2. Registra o álbum no banco de dados (Cloudflare KV)
         await fetch(`${WORKER_URL}/criar-album`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ albumId, links: linksDasImagens })
         });
 
-        // 3. Exibe o link do álbum e as miniaturas
         const linkDoAlbum = window.location.origin + `/album.html?id=${albumId}`;
         
         const divGrupo = document.createElement('div');
         divGrupo.className = 'item-upload';
-        // HTML limpo, sem o quadrado branco e texto direto
+   
         divGrupo.innerHTML = `
             <div class="galeria-preview">${miniaturasHTML}</div>
             <div class="controles-finais">
@@ -75,7 +70,6 @@ function fecharModal() {
     document.getElementById("modalVisualizacao").style.display = "none";
 }
 
-// Função de Cópia
 function copiarTexto(btn, texto) {
     navigator.clipboard.writeText(texto);
 
@@ -90,18 +84,15 @@ const corOriginal = btn.style.backgroundColor;
 }
 const overlay = document.getElementById('overlayDrop');
 
-// Exibe o quadrado quando arrastar para a tela
 window.addEventListener('dragover', (e) => {
     e.preventDefault();
     overlay.style.display = 'flex';
 });
 
-// Esconde se sair da tela
 window.addEventListener('dragleave', (e) => {
     if (e.relatedTarget === null) overlay.style.display = 'none';
 });
 
-// Esconde ao soltar e processa os arquivos
 window.addEventListener('drop', (e) => {
     e.preventDefault();
     overlay.style.display = 'none';
